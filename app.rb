ef8e5c78d9ade19224f30eb7e70492a4658e069c
destinybbrianna <destinybbrianna@gmail.com>
2018-08-22 21:01:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,34 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    erb :fortune
+  end 
+  
+  get '/results' do 
+    erb :results
+  end 
+  
+  post '/results' do 
+    @name = #{params[:name]}
+    @sign = params[:sign]
+    @yesorno = params[:yesorno]
+    
+  end 
+  post '/results' do 
+    @age = params[:age]
+  end 
+  
+  
+  
+end 
+ 
+# Add your post route and action below
+describe 'POST /results' do 
+    it 'responds with a 200' do
+      post '/results'
 
-  get '/food_form' do
-    erb :food_form
-  end
+      expect(last_response.status).to eq(200)
+    end
 
-  # Add your post route and action below
-
-end
+ 
